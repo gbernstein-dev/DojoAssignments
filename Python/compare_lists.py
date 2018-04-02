34b690c8b5dc74abd46d0dbd41cc7e496116ab12
GB-actualize <gbactualize@gmail.com>
2018-04-02 13:45:06 -0500

    compare lists

---
+++ Python/compare_lists.py
@@ -0,0 +1,7 @@
+def comparison(arr1,arr2):
+  if arr1 == arr2:
+    print "these are the same"
+  else:
+    print "these are different"
+comparison([1,2,3],[1,2,3])
+comparison([1,2,5,6,5],[1,2,5,6,5,3])
