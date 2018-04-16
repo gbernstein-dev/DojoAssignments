96de6d45cf5aa1479ec52fc0bf3895ddf7578dee
GB-actualize <gbactualize@gmail.com>
2018-04-16 11:59:50 -0500

    messing with surveys

--- Python/django/surveys/apps/survey_app/urls.py
+++ Python/django/surveys/apps/survey_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-  url(r'^survey$', views.index),
+  url(r'^survey/$', views.index),
+  url(r'^survey/process$', views.process),
   url(r'^result$', views.index)
 ]
--- Python/django/surveys/apps/survey_app/views.py
+++ Python/django/surveys/apps/survey_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-	if request.method == 'POST':
-		return render(request, 'survey_app/result.html', {'name':request.session['first_name'], 'location':request.session['dojo_location'], 'language':request.session['favorite_language'], 'comments':request.session['comment_section']})
-	else:
-		return render(request, 'survey_app/index.html')
+  return render(request, 'survey_app/index.html')
+
+def process(request):
+  context = {
+  request.session['first_name']: request.POST['first_name'],
+  request.session['dojo_location']: request.POST['dojo_location'],
+  request.session['favorite_language']: request.POST['favorite_language'],
+  request.session['comment_section']: request.POST['comment_section']
+  }
+  
+  print request.session['first_name']
+
+  return redirect(request,'/survey_app/result.html',context)
 
 
